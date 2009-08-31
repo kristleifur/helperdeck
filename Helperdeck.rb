2c9274fb0da8f717311985f0028aec67204c7580
Kristleifur Dadason <kristleifur@gmail.com>
2009-08-31 23:41:19 +0000

    Added the stage window & widget skeleton ... it actually correlates itself somewhat with the main board stuff so proper stage-board-bag click correlation is quite close now

--- Helperdeck.rb
+++ Helperdeck.rb
@@ -6,6 +6,7 @@ require 'Bag'
 require 'BoardSide'
 require 'Position'
 require 'SelectedPosition'
+require 'Stage'
 
 Shoes.app(:width => 480, :height => 320) do
 	Shoes.show_log()
@@ -68,7 +69,7 @@ Shoes.app(:width => 480, :height => 320) do
     debug "Saved"
   end
   
-  button "Load Board Posn's" do
+  @loadButton = button "Load Board Posn's" do
     debug "Load Board Posn's"
     freshPositions = YAML.load_file("boardTop.dump.yaml")
     @boardTop.positions = {}
@@ -98,7 +99,73 @@ Shoes.app(:width => 480, :height => 320) do
     debug "Loaded"
   end
   
+  # autoclick
+  begin
+    debug "Autoload Board Posn's"
+    
+    freshPositions = YAML.load_file("boardTop.dump.yaml")
+    @boardTop.positions = {}
+    freshPositions.values.each do | i |
+      if (@boardTop.positions[i.name])
+        debug "position #{i.name} is duplicated"
+      end
+      @boardTop.positions[i.name] = i
+    end
+    
+    # @boardTop.positions = YAML.load_file("boardTop.dump.yaml")
+    @boardTop.selectedPositions.clear()
+    @boardTop.hoverPositions.clear()
+    
+    freshPositions = YAML.load_file("boardBottom.dump.yaml")
+    @boardBottom.positions = {}
+    freshPositions.values.each do | i |
+      if (@boardBottom.positions[i.name])
+        debug "position #{i.name} is duplicated"
+      end
+      @boardBottom.positions[i.name] = i
+    end
+    
+    # @boardBottom.positions = YAML.load_file("boardBottom.dump.yaml")
+    @boardBottom.selectedPositions.clear()
+    @boardBottom.hoverPositions.clear()
+    debug "Loaded"
+  end
+  
   @selectedPosition = nil # Position.new()
+
+  @@stages = []
+  @@stageWindows = []
+  @@numberOfStages = 1
+  @@numberOfStages.times do | i |
+    tmpWin = window :title => "Stage", :width => 250, :height => 300 do
+      # ;
+    end
+    @@stageWindows << tmpWin
+    @@stages << BuildStage.new(@@stageWindows[-1], self)
+    debug @@stages[0]
+    
+  end
+  @@stages[0].stagename = "Power_in"
+  @@stageComponents = {}
+  @@stageComponents[@@stages[0].stagename] = @@stages[0].positions
+  debug "remember to sync the stageComponents lookup to the board positions post-load"
+  tmpStageComponents = %w(d10 d11 c28 r13 c29 r20)
+  tmpStageComponents.each do | i |
+    if @boardTop.positions[i] == nil && @boardBottom.positions[i] == nil
+      debug "#{i} not found on the boards, perhaps new it?"
+    else
+      pos ||= @boardTop.positions[i]
+      pos ||= @boardBottom.positions[i]
+      debug "Stages 0:"
+      debug @@stages[0]
+      debug "Stages 0 positions:"
+      debug @@stages[0].positions
+      debug "Pos:"
+      debug pos
+      @@stages[0].positions << pos
+    end
+  end
+  @@stages[0].update()
 
   # tehBag.components
     
