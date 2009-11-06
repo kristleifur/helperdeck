35ebd140447435bfaa76fafa359f5cf9e0437a0f
Eff2 <eff2@B3863E.local>
2009-11-06 19:24:04 +0000

    completed fixLongNames script, now run it and resave

--- fixLongnames.rb
+++ fixLongnames.rb
@@ -1,7 +1,12 @@
 #!/usr/bin/env ruby
 
-if ARGV != 2
+require 'component.rb'
+
+require 'yaml'
+
+if ARGV.size != 2
   puts "Usage: fixLongnames [goodYaml] [badYaml]"
+  exit
 end
 
 goodComponents = YAML::load_file(ARGV[0])
@@ -9,19 +14,33 @@ badComponents = YAML::load_file(ARGV[1])
 
 posNames = {}
 
-goodComponents.each do | goodPart |
-  goodPart.positions.each do | pos |
-    posNames[pos] ||= []
-    posNames[pos] << goodPart
+goodComponents.each do | bag |
+  bag.each do | goodPart |
+    goodPart.positions.each do | pos |
+      posNames[pos] ||= []
+      posNames[pos] << goodPart
+    end
   end
 end
 
-badComponents.each do | badPart |
-  puts "Long name: #{badPart.longname}"
-  puts "Short name(s):"
-  badPart.positions.each do | pos |
-    posNames[pos].each do | goodComponent |
-      puts goodComponent.longname
+badComponents.values.each do | bag |
+  bag.each do | badPart |
+    shortNames = {}
+    badPart.positions.each do | pos |
+      posNames[pos].each do | goodComponent |
+        shortNames[goodComponent.longname] ||= 0
+        shortNames[goodComponent.longname] += 1
+      end
+    end
+    if (shortNames.keys.size != 1)
+      puts "Long name: #{badPart.longname}"
+      puts "Funny! shortNames (#{shortNames.keys.size}): #{shortNames.keys} / #{shortNames.values}"
+    else
+      newname = shortNames.keys[0]
+      # if badPart.longname.size <= newname.size
+        puts "'#{badPart.longname}'   ->   '#{newname}'"
+        puts "\t#{badPart.longname.size}   ->   #{newname.size}"
+      # end
     end
   end
 end
