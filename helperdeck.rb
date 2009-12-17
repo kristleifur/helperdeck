8a9ae07c313bde56a6c9698347677e3bd0c9c994
Eff2 <eff2@B3863E.local>
2009-12-17 23:25:34 +0000

    Amp32 images and empty position YAML files

--- helperdeck.rb
+++ helperdeck.rb
@@ -94,209 +94,209 @@ Shoes.app(:width => 480, :height => 50) do
   end
   @selectedPosition = SelectedPosition.new(@liveComponentWin, self)
   
-	loadDatapack("amp15-ps")
+	loadDatapack("amp32")
 
  
-  @uglyBagModel = YAML::load_file("Amp15_BagsAndComponents.yaml")
-  # debug "bags: #{@uglyBagModel.keys()}"
-
-  @bagControllers = {}
-  @bagWins = {}
-
-  @uglyBagModel.keys.each do | i |
-    winHeight = 19 + 21 * (@uglyBagModel[i].size + 1)
-    bagWin = window :title => "Bag #{i}, #{@uglyBagModel[i].size} pieces", :width => 340, :height => winHeight do
-      #
-    end
-    @bagWins[i] = bagWin
-    tehBag = Bag.new(bagWin, self, "#{i}")
-    tehBag.components = @uglyBagModel[i]
-    @bagControllers[i] = tehBag
-    tehBag.update()
-  end
+  # @uglyBagModel = YAML::load_file("Amp15_BagsAndComponents.yaml")
+  #   # debug "bags: #{@uglyBagModel.keys()}"
+  # 
+  #   @bagControllers = {}
+  #   @bagWins = {}
+  # 
+  #   @uglyBagModel.keys.each do | i |
+  #     winHeight = 19 + 21 * (@uglyBagModel[i].size + 1)
+  #     bagWin = window :title => "Bag #{i}, #{@uglyBagModel[i].size} pieces", :width => 340, :height => winHeight do
+  #       #
+  #     end
+  #     @bagWins[i] = bagWin
+  #     tehBag = Bag.new(bagWin, self, "#{i}")
+  #     tehBag.components = @uglyBagModel[i]
+  #     @bagControllers[i] = tehBag
+  #     tehBag.update()
+  #   end
   
   button "DRAW" do
-    debug "Enter Draw"
-    @boardTop.drawMode()
-    @boardBottom.drawMode()
-		@powerSchematic.drawMode()
+    debug "Enter Draw - disabled!"
+    #     @boardTop.drawMode()
+    #     @boardBottom.drawMode()
+    # @powerSchematic.drawMode()
   end
   
   button "SELECT" do
-    debug "Entering select mode"
-    @boardTop.selectMode()
-    @boardBottom.selectMode()
-		@powerSchematic.selectMode()
+    debug "Entering select mode - disabled!"
+    #     @boardTop.selectMode()
+    #     @boardBottom.selectMode()
+    # @powerSchematic.selectMode()
   end
   
   button "Save Board Posn's" do
-    debug "Save Board Posn's"
-    componentYaml = @boardTop.positions.to_yaml()
-    # debug componentYaml
-    File.open("boardTop.dump.yaml", "w") do | f |
-      f.puts componentYaml
-    end
-    componentYaml = @boardBottom.positions.to_yaml()
-    # debug componentYaml
-    File.open("boardBottom.dump.yaml", "w") do | f |
-      f.puts componentYaml
-    end
-    debug "Saved"
-		componentYaml = @powerSchematic.positions.to_yaml()
-		File.open("PowerSchematic.dump.yaml", "w") do | f |
-			f.puts componentYaml
-		end
+    debug "Save Board Posn's - disabled!"
+    #     componentYaml = @boardTop.positions.to_yaml()
+    #     # debug componentYaml
+    #     File.open("boardTop.dump.yaml", "w") do | f |
+    #       f.puts componentYaml
+    #     end
+    #     componentYaml = @boardBottom.positions.to_yaml()
+    #     # debug componentYaml
+    #     File.open("boardBottom.dump.yaml", "w") do | f |
+    #       f.puts componentYaml
+    #     end
+    #     debug "Saved"
+    # componentYaml = @powerSchematic.positions.to_yaml()
+    # File.open("PowerSchematic.dump.yaml", "w") do | f |
+    #   f.puts componentYaml
+    # end
   end
   
   @loadButton = button "Load Board Posn's" do
-    debug "Load Board Posn's"
-    freshPositions = YAML.load_file("boardTop.dump.yaml")
-    @boardTop.positions = {}
-    freshPositions.values.each do | i |
-      if (@boardTop.positions[i.name])
-        debug "position #{i.name} is duplicated"
-      end
-      @boardTop.positions[i.name] = i
-    end
-    
-    # @boardTop.positions = YAML.load_file("boardTop.dump.yaml")
-    @boardTop.selectedPositions.clear()
-    @boardTop.hoverPositions.clear()
-    
-    freshPositions = YAML.load_file("boardBottom.dump.yaml")
-    @boardBottom.positions = {}
-    freshPositions.values.each do | i |
-      if (@boardBottom.positions[i.name])
-        debug "position #{i.name} is duplicated"
-      end
-      @boardBottom.positions[i.name] = i
-    end
-    
-    # @boardBottom.positions = YAML.load_file("boardBottom.dump.yaml")
-    @boardBottom.selectedPositions.clear()
-    @boardBottom.hoverPositions.clear()
-
-		freshPositions = YAML.load_file("PowerSchematic.dump.yaml")
-    @powerSchematic.positions = {}
-    freshPositions.values.each do | i |
-      if (@powerSchematic.positions[i.name])
-        debug "position #{i.name} is duplicated"
-      end
-      @powerSchematic.positions[i.name] = i
-    end
-    
-    # @boardBottom.positions = YAML.load_file("boardBottom.dump.yaml")
-    @powerSchematic.selectedPositions.clear()
-    @powerSchematic.hoverPositions.clear()
-
-    debug "Loaded"
+    # debug "Load Board Posn's"
+    #     freshPositions = YAML.load_file("boardTop.dump.yaml")
+    #     @boardTop.positions = {}
+    #     freshPositions.values.each do | i |
+    #       if (@boardTop.positions[i.name])
+    #         debug "position #{i.name} is duplicated"
+    #       end
+    #       @boardTop.positions[i.name] = i
+    #     end
+    #     
+    #     # @boardTop.positions = YAML.load_file("boardTop.dump.yaml")
+    #     @boardTop.selectedPositions.clear()
+    #     @boardTop.hoverPositions.clear()
+    #     
+    #     freshPositions = YAML.load_file("boardBottom.dump.yaml")
+    #     @boardBottom.positions = {}
+    #     freshPositions.values.each do | i |
+    #       if (@boardBottom.positions[i.name])
+    #         debug "position #{i.name} is duplicated"
+    #       end
+    #       @boardBottom.positions[i.name] = i
+    #     end
+    #     
+    #     # @boardBottom.positions = YAML.load_file("boardBottom.dump.yaml")
+    #     @boardBottom.selectedPositions.clear()
+    #     @boardBottom.hoverPositions.clear()
+    # 
+    #     freshPositions = YAML.load_file("PowerSchematic.dump.yaml")
+    #     @powerSchematic.positions = {}
+    #     freshPositions.values.each do | i |
+    #       if (@powerSchematic.positions[i.name])
+    #         debug "position #{i.name} is duplicated"
+    #       end
+    #       @powerSchematic.positions[i.name] = i
+    #     end
+    #     
+    #     # @boardBottom.positions = YAML.load_file("boardBottom.dump.yaml")
+    #     @powerSchematic.selectedPositions.clear()
+    #     @powerSchematic.hoverPositions.clear()
+    # 
+    #     debug "Loaded"
   end
   
   # autoclick
   begin
-    debug "Autoload Board Posn's"
-    
-    freshPositions = YAML.load_file("boardTop.dump.yaml")
-    @boardTop.positions = {}
-    freshPositions.values.each do | i |
-      if (@boardTop.positions[i.name])
-        debug "position #{i.name} is duplicated"
-      end
-      @boardTop.positions[i.name] = i
-    end
-    
-    # @boardTop.positions = YAML.load_file("boardTop.dump.yaml")
-    @boardTop.selectedPositions.clear()
-    @boardTop.hoverPositions.clear()
-    
-    freshPositions = YAML.load_file("boardBottom.dump.yaml")
-    @boardBottom.positions = {}
-    freshPositions.values.each do | i |
-      if (@boardBottom.positions[i.name])
-        debug "position #{i.name} is duplicated"
-      end
-      @boardBottom.positions[i.name] = i
-    end
-    
-    # @boardBottom.positions = YAML.load_file("boardBottom.dump.yaml")
-    @boardBottom.selectedPositions.clear()
-    @boardBottom.hoverPositions.clear()
-    
-    freshPositions = YAML.load_file("PowerSchematic.dump.yaml")
-    @powerSchematic.positions = {}
-    freshPositions.values.each do | i |
-      if (@powerSchematic.positions[i.name])
-        debug "position #{i.name} is duplicated"
-      end
-      @powerSchematic.positions[i.name] = i
-    end
-    
-    # @boardBottom.positions = YAML.load_file("boardBottom.dump.yaml")
-    @powerSchematic.selectedPositions.clear()
-    @powerSchematic.hoverPositions.clear()
-		
-    debug "Loaded"
-
-    debug "Entering select mode"
-    @boardTop.selectMode()
-    @boardBottom.selectMode()
-		@powerSchematic.selectMode()
+    # debug "Autoload Board Posn's"
+    #     
+    #     freshPositions = YAML.load_file("boardTop.dump.yaml")
+    #     @boardTop.positions = {}
+    #     freshPositions.values.each do | i |
+    #       if (@boardTop.positions[i.name])
+    #         debug "position #{i.name} is duplicated"
+    #       end
+    #       @boardTop.positions[i.name] = i
+    #     end
+    #     
+    #     # @boardTop.positions = YAML.load_file("boardTop.dump.yaml")
+    #     @boardTop.selectedPositions.clear()
+    #     @boardTop.hoverPositions.clear()
+    #     
+    #     freshPositions = YAML.load_file("boardBottom.dump.yaml")
+    #     @boardBottom.positions = {}
+    #     freshPositions.values.each do | i |
+    #       if (@boardBottom.positions[i.name])
+    #         debug "position #{i.name} is duplicated"
+    #       end
+    #       @boardBottom.positions[i.name] = i
+    #     end
+    #     
+    #     # @boardBottom.positions = YAML.load_file("boardBottom.dump.yaml")
+    #     @boardBottom.selectedPositions.clear()
+    #     @boardBottom.hoverPositions.clear()
+    #     
+    #     freshPositions = YAML.load_file("PowerSchematic.dump.yaml")
+    #     @powerSchematic.positions = {}
+    #     freshPositions.values.each do | i |
+    #       if (@powerSchematic.positions[i.name])
+    #         debug "position #{i.name} is duplicated"
+    #       end
+    #       @powerSchematic.positions[i.name] = i
+    #     end
+    #     
+    #     # @boardBottom.positions = YAML.load_file("boardBottom.dump.yaml")
+    #     @powerSchematic.selectedPositions.clear()
+    #     @powerSchematic.hoverPositions.clear()
+    #     
+    #     debug "Loaded"
+    # 
+    #     debug "Entering select mode"
+    #     @boardTop.selectMode()
+    #     @boardBottom.selectMode()
+    #     @powerSchematic.selectMode()
   end
   
   @selectedPosition = nil # Position.new() #TODO: fix / use / toss
 
-  @stages = []
-  @stageWindows = []
-  @stageComponents = {}
-
-  @numberOfStages = 5
-  @numberOfStages.times do | i |
-    tmpWin = window :title => "Stage #{i}", :width => 250, :height => 60 do
-      # ;
-    end
-    @stageWindows << tmpWin
-    @stages << BuildStage.new(tmpWin, self)
-    # debug @stages[0]
-    # @stageComponents[@stages[i].stagename] = @stages[i].positions
-  end
-  @stages[0].stagename = "Power_in"
-  @stages[1].stagename = "8v"
-  @stages[2].stagename = "5v"
-  @stages[3].stagename = "vpp+14v"
-  @stages[4].stagename = "vpp+9v"
-  
-  debug "Developer note: remember to sync the stageComponents lookup to the board positions post-load"
-  tmpStageComponents = {}
-  tmpStageComponents[0] = %w(d10 d11 c28 r13 c29 r20)
-  tmpStageComponents[1] = %w(d8 r41 c33 r42 q3 d4 c36)
-  tmpStageComponents[2] = %w(l5 c41 c37 ic3 r47 c38 r48)
-  tmpStageComponents[3] = %w(d9 r44 c34 r45 d5 q4 c35)
-  tmpStageComponents[4] = %w(ic2 c10)
-  # debug tmpStageComponents
-  #   debug tmpStageComponents[0]
-  #   debug tmpStageComponents[0].class()
-  tmpStageComponents.each do | stageNr, stage |
-    # debug "Populating stage '#{stageNr}'"
-    stage.each do | i |
-      # debug "Looking for position '#{i}'"
-      if @boardTop.positions[i] == nil && @boardBottom.positions[i] == nil
-        debug "Stage init - warning - position #{i} not found on the board"
-      else
-        pos ||= @boardTop.positions[i]
-        pos ||= @boardBottom.positions[i]
-        # debug "Stages 0:"
-        # debug @stages[0]
-        # debug "Stages 0 positions:"
-        # debug @stages[0].positions
-        # debug "Pos:"
-        # debug pos
-        # debug "Putting pos '#{pos}' into @stages[#{stageNr}]"
-        @stages[stageNr].positions << pos
-      end
-    end
-    # debug "Updating stage nr. #{stageNr}, name '#{@stages[stageNr].stagename}'"
-    @stages[stageNr].update()
-  end
+  # @stages = []
+  #  @stageWindows = []
+  #  @stageComponents = {}
+  # 
+  #  @numberOfStages = 5
+  #  @numberOfStages.times do | i |
+  #    tmpWin = window :title => "Stage #{i}", :width => 250, :height => 60 do
+  #      # ;
+  #    end
+  #    @stageWindows << tmpWin
+  #    @stages << BuildStage.new(tmpWin, self)
+  #    # debug @stages[0]
+  #    # @stageComponents[@stages[i].stagename] = @stages[i].positions
+  #  end
+  #  @stages[0].stagename = "Power_in"
+  #  @stages[1].stagename = "8v"
+  #  @stages[2].stagename = "5v"
+  #  @stages[3].stagename = "vpp+14v"
+  #  @stages[4].stagename = "vpp+9v"
+  #  
+  #  debug "Developer note: remember to sync the stageComponents lookup to the board positions post-load"
+  #  tmpStageComponents = {}
+  #  tmpStageComponents[0] = %w(d10 d11 c28 r13 c29 r20)
+  #  tmpStageComponents[1] = %w(d8 r41 c33 r42 q3 d4 c36)
+  #  tmpStageComponents[2] = %w(l5 c41 c37 ic3 r47 c38 r48)
+  #  tmpStageComponents[3] = %w(d9 r44 c34 r45 d5 q4 c35)
+  #  tmpStageComponents[4] = %w(ic2 c10)
+  #  # debug tmpStageComponents
+  #  #   debug tmpStageComponents[0]
+  #  #   debug tmpStageComponents[0].class()
+  #  tmpStageComponents.each do | stageNr, stage |
+  #    # debug "Populating stage '#{stageNr}'"
+  #    stage.each do | i |
+  #      # debug "Looking for position '#{i}'"
+  #      if @boardTop.positions[i] == nil && @boardBottom.positions[i] == nil
+  #        debug "Stage init - warning - position #{i} not found on the board"
+  #      else
+  #        pos ||= @boardTop.positions[i]
+  #        pos ||= @boardBottom.positions[i]
+  #        # debug "Stages 0:"
+  #        # debug @stages[0]
+  #        # debug "Stages 0 positions:"
+  #        # debug @stages[0].positions
+  #        # debug "Pos:"
+  #        # debug pos
+  #        # debug "Putting pos '#{pos}' into @stages[#{stageNr}]"
+  #        @stages[stageNr].positions << pos
+  #      end
+  #    end
+  #    # debug "Updating stage nr. #{stageNr}, name '#{@stages[stageNr].stagename}'"
+  #    @stages[stageNr].update()
+  #  end
   # @stages[0].update()
 
   # tehBag.components
