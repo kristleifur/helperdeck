16c44c6d67b1979ff8d554fcf6e3509f3e0894c8
Eff2 <eff2@B3863E.local>
2010-02-28 00:16:41 +0000

    Hacking on the unibag window

--- bagswindow.rb
+++ bagswindow.rb
@@ -6,24 +6,33 @@ class BagsWindow
   def initialize(win, bagmodel)
     @win = win
     @bagmodel = bagmodel
-    @bigflow = @win.flow # do | flow |
     @stacks = {} 
-    bagmodel.each do | bagname, bag |
-      @stacks[bagname] = @win.stack do | stack |
-        link = @win.link "POFF"
-        link.click do
-          @win.animate(10) do 
-            # debug "Po"
-            @stacks[bagname].height -= 3
+    @stacks_hidden = {}
+    @bigflow = @win.stack do
+      bagmodel.each do | bagname, bag |
+        # debug bagname
+        # debug bag.to_yaml()
+        @stacks_hidden[bagname] = true
+        @stacks[bagname] = @win.flow :width => "1" do
+          link = @win.link bagname
+          link.click do
+            if @stacks_hidden[bagname]
+              @stacks[bagname].width = "100%"
+              @stacks[bagname].height = "100%"
+              debug "stack (#{@stacks[bagname].width} x #{@stacks[bagname].height})"
+            else
+              @stacks[bagname].width = "1"
+              @stacks[bagname].height = "1"
+              debug "stack (#{@stacks[bagname].width} x #{@stacks[bagname].height})"
+            end
+            @stacks_hidden[bagname] = !@stacks_hidden[bagname]
           end
-          
-          debug "gliggs"
-          # @stacks[bagname]= @win.stack do | stack |
-          #   @win.para "poff"
-          # end
+          @win.para link
+          # @win.para "Lorem ipsum constituet sic dolor argadrg adgr adrg adrg adrg adrgar\n `oregEOPS;GIPOEJSG LOLOLL" #bag.to_yaml().split("\n")[0...3]
+          "Lorem ipsum constituet sic dolor argadrg adgr adrg adrg adrg adrgar\n `oregEOPS;GIPOEJSG LOLOLL".split(" ").each do |pg|
+            @win.para pg
+          end
         end
-        @win.para link
-        @win.para bag.to_yaml().split("\n")[0...3]
       end
     end
   end
